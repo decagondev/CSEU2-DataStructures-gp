f10ed5e35247165fac0765e6d52db9d87ccdf3c5
Tom <tomtarpeydev@gmail.com>
2019-10-14 11:16:18 +0100

    day 1 code

--- src/day1/doubly_linked_list/doubly_linked_list.py
+++ src/day1/doubly_linked_list/doubly_linked_list.py
@@ -82,7 +82,7 @@ class DoublyLinkedList:
         # return the value
         return value
 
-   """Wraps the given value in a ListNode and inserts it 
+    """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
@@ -109,23 +109,103 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        pass
+        # set the return value to the tail value
+        value = self.tail.value
+        # delete the tail
+        self.delete(self.tail)
+        # return the value
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        # if node is head then return
+        if node is self.head:
+            return
+        # set value to nodes value
+        value = node.value
+        # if the node is the tail
+        if node is self.tail:
+            # remove node from tail
+            self.remove_from_tail()
+        # otherwise
+        else:
+            # delete the node
+            node.delete()
+            # decrement the length
+            self.length -= 1
+
+        # add the value to the head
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+                # if node is tail then return
+        if node is self.tail:
+            return
+        # set value to nodes value
+        value = node.value
+        # if the node is the head
+        if node is self.head:
+            # remove node from head
+            self.remove_from_head()
+        # otherwise
+        else:
+            # delete the node
+            node.delete()
+            # decrement the length
+            self.length -= 1
+
+        # add the value to the tail
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        # decrement the length
+        self.length -= 1
+        # if not head or tail return
+        if not self.head and not self.tail:
+            return
+        # if the head is the tail
+        if self.head == self.tail:
+            # set the head and tail to none
+            self.head = None
+            self.tail = None
+        # otherwise if head is the node
+        elif self.head == node:
+            # set the head to the nodes next
+            self.head = node.next
+            # delete node
+            node.delete()
+        # otherwise if tail is the node
+        elif self.tail == node:
+            # set the tail to the nodes prev
+            self.tail = node.prev
+            # delete node
+            node.delete()
+        # otherwise
+        else:
+            # delete node
+            node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        # if no head the return None
+        if not self.head:
+            return None
+        # set the initial max value to the heads value
+        max_value = self.head.value
+        # set current node to head
+        current_node = self.head
+        # loop over nodes while current exists
+        while current_node:
+            # if current value is greater than the max value
+            if current_node.value > max_value:
+                # set the max value to the current value
+                max_value = current_node.value
+            # move to the next node
+            current_node = current_node.next
+        # return max value
+        return max_value
