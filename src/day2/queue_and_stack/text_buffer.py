7d7b4cf78845154bd65acce4d6d7e78605e83fc4
Tom <tomtarpeydev@gmail.com>
2019-10-15 10:29:55 +0100

    text buffer

---
+++ src/day2/queue_and_stack/text_buffer.py
@@ -0,0 +1,30 @@
+from doubly_linked_list import DoublyLinkedList 
+
+class TextBuffer:
+ 
+    def __init__(self, init=None):
+        pass
+
+    def __str__(self):
+        pass
+
+    def append(self, string_to_add):
+        pass
+    
+    def prepend(self, string_to_add):
+        pass
+
+    def delete_front(self, chars_to_remove):
+        pass
+
+    def delete_back(self, chars_to_remove):
+        pass
+
+    """
+    Join other_buffer to self
+    The input buffer gets concatenated to the end of this buffer 
+    The tail of the concatenated buffer will be the tail of the other buffer 
+    The head of the concatenated buffer will be the head of this buffer 
+    """
+    def join(self, other_buffer):
+        pass
